6a57370a48591086740d896b0f7330032f16699e
unknown <nazrulislam66188@gmail.com>
2023-09-16 12:11:48 +0600

    navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import LoginPage from "./scenes/loginPage"
 import HomePage from "./scenes/homePage"
 import ProfilePage from "./scenes/profilePage"
+import { useSelector } from "react-redux"
+import { useMemo } from "react"
+import { CssBaseline, createTheme } from "@mui/material"
+import { themeSettings } from "./theme"
+import { ThemeProvider } from "@emotion/react"
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
   return (
     <div className='app'>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/profile/:userId" element={<ProfilePage />} />
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   )
